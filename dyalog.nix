c985b324f3ee9b1226ad7e4b04398a4d613e6bbd
TomaSajt <62384384+TomaSajt@users.noreply.github.com>
2023-06-03 01:51:07 +0200

    Refactor Dyalog derivation

--- dyalog.nix
+++ dyalog.nix
@@ -16,21 +16,24 @@
 , unixODBC
 , xorg
 }:
-
-stdenv.mkDerivation rec {
-  src = fetchurl {
-    url = "https://download.dyalog.com/download.php?file=${shortVersion}/linux_64_${version}_unicode.x86_64.deb";
-    sha256 = "sha256-pA/WGTA6YvwG4MgqbiPBLKSKPtLGQM7BzK6Bmyz5pmM=";
-  };
-
-  name = "dyalog-${version}";
+let
+  pname = "dyalog";
   version = "18.2.45405";
 
   shortVersion = lib.concatStringsSep "." (lib.take 2 (lib.splitString "." version));
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  src = fetchurl {
+    url = "https://download.dyalog.com/download.php?file=${shortVersion}/linux_64_${version}_unicode.x86_64.deb";
+    sha256 = "sha256-pA/WGTA6YvwG4MgqbiPBLKSKPtLGQM7BzK6Bmyz5pmM=";
+  };
+in
+stdenv.mkDerivation {
+
+  inherit pname version src;
 
   buildInputs = [
+    autoPatchelfHook
+
     dpkg
 
     alsaLib
@@ -54,4 +57,12 @@ stdenv.mkDerivation rec {
     ln -s $out/dyalog/dyalog $out/bin/dyalog
     ln -s $out/dyalog/mapl $out/bin/mapl
   '';
+
+  meta = with lib; {
+    description = "Dyalog APL";
+    homepage = "https://www.dyalog.com";
+    license = licenses.unfree;
+    maintainers = with maintainers; [ tomasajt ];
+    platforms = [ "x86_64-linux" ];
+  };
 }
